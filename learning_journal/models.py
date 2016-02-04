e649ab3272ec6bfcd93efa5b8670a0b06627bacf
Gabriel Meringolo <gabriel.meringolo@gmail.com>
2016-02-04 00:36:16 -0800

    Added title, body, created and edited columns, need to limit length of title

--- learning_journal/models.py
+++ learning_journal/models.py
@@ -3,6 +3,8 @@ from sqlalchemy import (
     Index,
     Integer,
     Text,
+    DateTime,
+    func,
     )
 
 from sqlalchemy.ext.declarative import declarative_base
@@ -14,8 +16,10 @@ from sqlalchemy.orm import (
 
 from zope.sqlalchemy import ZopeTransactionExtension
 
+
 DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
 Base = declarative_base()
+
 
 
 class MyModel(Base):
@@ -30,14 +34,15 @@ Index('my_index', MyModel.name, unique=True, mysql_length=255)
 class Entry(Base):
     __tablename__ = 'entries'
     id = Column(Integer, primary_key=True)
-    title = Column(Text(255),primary_key=True)
+    title = Column(Text, mysql_length=255, unique=True) # need to figure out how to limit length properly
     body = Column(Text)
-    created = Column(Integer)
-    edited = Column(Integer)
+    created = Column(DateTime(timezone=True), default=func.now())
+    edited = Column(DateTime(timezone=True), default=func.now(), onupdate=func.now())
 
+Index('Entry_Index', Entry.title, unique=True, mysql_length=255)
 
-    def all(self):
-        pass
-
-    def by_id(self):
-        pass
+    # def all(self):
+    #     pass
+    #
+    # def by_id(self):
+    #     pass
