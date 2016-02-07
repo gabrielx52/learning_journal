64900afba37cf72067ce37f087736cd6fe854b58
Gabriel Meringolo <gabriel.meringolo@gmail.com>
2016-02-07 11:52:29 -0800

    set char length using dialect char, working on timezone

--- learning_journal/models.py
+++ learning_journal/models.py
@@ -5,16 +5,26 @@ from sqlalchemy import (
     Text,
     DateTime,
     func,
-    String,
-    Unicode
+
     )
+
+from datetime import datetime
+#now = datetime.now()
+from pytz import timezone
+
+from sqlalchemy.dialects.sqlite import (
+    CHAR,
+    DATETIME,
+    )
+
 
 from sqlalchemy.ext.declarative import declarative_base
 
 from sqlalchemy.orm import (
     scoped_session,
     sessionmaker,
-    validates
+    validates,
+    Session,
     )
 
 from zope.sqlalchemy import ZopeTransactionExtension
@@ -23,7 +33,9 @@ from zope.sqlalchemy import ZopeTransactionExtension
 DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
 Base = declarative_base()
 
-
+def time_str(time_obj):
+    time_str = str(datetime.now)
+    return time_str()
 
 class MyModel(Base):
     __tablename__ = 'models'
@@ -37,22 +49,39 @@ Index('my_index', MyModel.name, unique=True, mysql_length=255)
 class Entry(Base):
     __tablename__ = 'entries'
     id = Column(Integer, primary_key=True)
-    title = Column(Text, unique=True) # need to figure out how to limit length properly
+    title = Column(CHAR(255), unique=True) # unicode supports char length
     body = Column(Text)
-    created = Column(DateTime(timezone=True), default=func.now())
-    edited = Column(DateTime(timezone=True), default=func.now(), onupdate=func.now())
+    # created = Column(Text, default=(time_str(datetime.now)))
+    # edited = Column(Text,default=time_str(datetime.now), onupdate=time_str(datetime.now))
+    created = Column(DATETIME, default=func.now())
+    edited = Column(DATETIME(timezone=True), onupdate=datetime.now())
 
-    @validates('title')
-    def validate_title(self, key, title):
-        assert len(title) <= 255
-        return title
+    # def __init__(self, id, title, body, created, edited):
+    #     self.id = id
+    #     self.title = title
+    #     self.body = body
+    #     self.created = 'nuts'
+    #     self.edited = timezone('US/Pacific').localize(edited)
 
-    @classmethod
-    def all(cls):
-        pass
+    # @validates('created')
+    # def validate_created(self, key, created):
+    #     assert
 
-    @classmethod
-    def by_id(cls):
-        pass
+
+    # @validates('title')
+    # def validate_title(self, key, title):
+    #     assert len(title) <= 255, 'Title must be less than 255 characters'
+    #     return title
+
+
+    # @classmethod
+    # def all(cls):
+    #     pass
+    #
+    # @classmethod
+    # def by_id(cls, id):
+    #     return Session.query(id).filter(id=id)
 
 Index('Entry_Index', Entry.title, unique=True, mysql_length=255)
+
+
