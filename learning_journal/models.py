be3922f9a52118274f0f3cfa444566574dd966b7
Gabriel Meringolo <gabriel.meringolo@gmail.com>
2016-02-11 09:53:08 -0800

    used provided models.py file

--- learning_journal/models.py
+++ learning_journal/models.py
@@ -1,38 +1,25 @@
+import datetime
 from sqlalchemy import (
     Column,
+    DateTime,
     Index,
     Integer,
     Text,
-    func,
-    MetaData,
-    desc,
+    Unicode,
+    UnicodeText,
     )
 
-
-
-from sqlalchemy.dialects.sqlite import (
-    CHAR,
-    DATETIME,
-    )
-
-
+import sqlalchemy as sa
 from sqlalchemy.ext.declarative import declarative_base
 
 from sqlalchemy.orm import (
     scoped_session,
     sessionmaker,
-    validates,
-    Session,
-    query,
-    object_session,
     )
 
 from zope.sqlalchemy import ZopeTransactionExtension
 
-
 DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
-
-
 Base = declarative_base()
 
 
@@ -48,43 +35,27 @@ Index('my_index', MyModel.name, unique=True, mysql_length=255)
 class Entry(Base):
     __tablename__ = 'entries'
     id = Column(Integer, primary_key=True)
-    title = Column(CHAR(255), unique=True) # unicode supports char length?
-    body = Column(Text)
-    created = Column(DATETIME(timezone=True), default=func.now())
-    edited = Column(DATETIME(timezone=True),default=func.now(), onupdate=func.now())
+    title = Column(Unicode(255), unique=True, nullable=False)
+    body = Column(UnicodeText, default=u'')
+    created = Column(DateTime, default=datetime.datetime.utcnow)
+    edited = Column(DateTime, default=datetime.datetime.utcnow)
 
     @classmethod
-    def by_id(cls, DB, id):
-        try:
-            id_query = (DB.query(cls).get(id))
-            return[id_query.id, id_query.title, id_query.body, id_query.created, id_query.edited]
-        except TypeError:
-            return 'Requires two positional arguments: database and id number'
+    def all(cls, session=None):
+        """return a query with all entries, ordered by creation date reversed
+        """
+        if session is None:
+            session = DBSession
+        return session.query(cls).order_by(sa.desc(cls.created)).all()
 
     @classmethod
-    def all(cls, DB):
-        try:
-            db_query = DB.query(cls).order_by(desc(cls.created))
-            return [(column.id, column.title, column.body, column.created, column.edited) for column in db_query]
-        except TypeError:
-            return 'Requires one positional argument for database'
+    def by_id(cls, id, session=None):
+        """return a single entry identified by id
+        If no entry exists with the provided id, return None
+        """
+        if session is None:
+            session = DBSession
+        return session.query(cls).get(id)
 
-
-'''
-The entry class should support a classmethod all that returns all the entries in the database,
-ordered so that the most recent entry is first.
-The entry class should support a classmethod by_id that returns a single entry, given an id.
-
-'''
-
-Index('Entry_Index', Entry.title, unique=True, mysql_length=255)
-
-
-
-
-
-    # @validates('title')
-    # def validate_title(self, key, title):
-    #     assert len(title) <= 255, 'Title must be less than 255 characters'
-    #     return title
-
+#def __repr__(self):
+#    return '{}{}{}{}'.format(n.id, n.title,...,...)
